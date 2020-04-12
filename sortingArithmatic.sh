1733ad704f47342ab885c92096717087e67f836f
DipaliBachhav <dipa2718@gmail.com>
2020-04-12 19:03:42 +0530

    Added User Inputs

--- sortingArithmatic.sh
+++ sortingArithmatic.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -x
 echo "Arithamtic Computation And Sorting"
 
+#Take User Inputs a,b,c
+read -p "Enter The value Of a := " a
+read -p "Enter The Value Of b := " b
+read -p "Enter The Value Of c := " c
+
